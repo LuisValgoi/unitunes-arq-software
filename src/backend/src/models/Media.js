0a72b988be5567c881db00fa7453825913a474c5
Luis Valgoi <luis.valgoi@sap.com>
2019-11-05 16:35:21 -0300

    added missing fks

--- src/backend/src/models/Media.js
+++ src/backend/src/models/Media.js
@@ -5,12 +5,12 @@ const MediaSchema = new Schema({
   description: { type: String, required: true, max: 1000 },
   price: { type: Number, required: true },
   category: { type: String, required: true },
-  authors: [{ type: Schema.Types.ObjectId, ref: "Academic", required: true }],
+  author: [{ type: Schema.Types.ObjectId, ref: "Academic", required: true }],
+  classification: { type: Schema.Types.ObjectId, ref: "Classification" },
   content: Buffer,
-  classificationValue: Number,
+  image: Buffer,
   isApropriated: Boolean,
   isPaid: Boolean,
-  image: Buffer,
 }, {
   discriminatorKey: "type",
   timestamps: true
--- src/backend/src/models/Movimentation.js
+++ src/backend/src/models/Movimentation.js
@@ -1,10 +1,11 @@
 const { Schema, model } = require("mongoose");
 
 const MovimentationSchema = new Schema({
-  description: String,
-  amount: Number,
+  amount: { type: Number, required: true },
   seller: { type: Schema.Types.ObjectId, ref: "Academic" },
-  buyer: { type: Schema.Types.ObjectId, ref: "Academic" }
+  buyer: { type: Schema.Types.ObjectId, ref: "Academic" },
+  medias: [{ type: Schema.Types.ObjectId, ref: "Media" }],
+  description: String
 }, {
   timestamps: true
 });
