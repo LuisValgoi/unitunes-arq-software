f257b70e1e427053810db76c3469776c3eaaeea1
Luis Valgoi <luis.valgoi@sap.com>
2019-11-05 15:44:42 -0300

    added university model

---
+++ src/backend/src/models/University.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require("mongoose");
+
+const UniversitySchema = new Schema({
+  name: { type: String, required: true, max: 255 },
+  country: { type: String, required: true, max: 255 },
+  state: { type: String, required: true, max: 2 },
+  address: { type: String, required: false, max: 255 }
+}, {
+  timestamps: true
+});
+module.exports = model("University", UniversitySchema);
