d2020f6822e7909d5d4d4289844258e2f15d742f
Luis Valgoi <luis.valgoi@sap.com>
2019-11-05 19:33:10 -0300

    added mongodb connection

--- src/backend/src/server.js
+++ src/backend/src/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 const cors = require("cors");
 const server = express();
 
+mongoose.connect("mongodb+srv://Global:eHlJfs2G9SJvQwjC@cluster0-imsf4.mongodb.net/unitunes?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 server.use(cors());
 server.use(express.json());
 server.use(routes);
