c6d92aa109fbee0371eb75358d3d882d3b88cefb
Luis Valgoi <luis.valgoi@sap.com>
2019-11-05 15:48:11 -0300

    added album entity

---
+++ src/backend/src/models/Album.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+
+const AlbumSchema = new Schema({
+  name: { type: String, required: true, max: 255 },
+  description: { type: String, required: true, max: 255 },
+  medias: [{ type: Schema.Types.ObjectId, ref: "Media" }]
+}, {
+  timestamps: true
+});
+module.exports = model("Album", AlbumSchema);
