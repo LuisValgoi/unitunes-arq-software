526b604702dee23323fdd5ca858c955ffd6dab1b
Luis Valgoi <luis.valgoi@sap.com>
2019-11-06 00:03:39 -0300

    added all request method types for university entity

--- src/backend/src/controllers/UniversityController.js
+++ src/backend/src/controllers/UniversityController.js
@@ -5,15 +5,41 @@ module.exports = {
     return res.json(await University.find());
   },
 
+  async getById (req, res) {
+    const { id } = req.params;
+    const response = await University.findById(id)
+    return res.json(response);
+  },
+
   async insert (req, res) {
     const { name, country, state, address } = req.body;
-    const univesity = await University.create({
+    const record = await University.create({
       name,
       country,
       state,
       address,
     });
 
-    return res.json(univesity)
+    return res.json(record)
+  },
+
+  async update (req, res) {
+    const { id } = req.params;
+    const { name, country, state, address } = req.body;
+    const record = await University.findByIdAndUpdate(id, { 
+      name, 
+      country, 
+      state, 
+      address 
+    }, { new: true });
+
+    return res.json(record);
+  },
+
+  async remove (req, res) {
+    const { id } = req.params;
+    const record = await University.findByIdAndDelete(id);
+
+    return res.json(record);
   }
 };
--- src/backend/src/routes.js
+++ src/backend/src/routes.js
@@ -4,6 +4,9 @@ const UniversityController = require("../src/controllers/UniversityController");
 const routes = express.Router();
 
 routes.get("/university", UniversityController.getAll);
+routes.get("/university/:id", UniversityController.getById);
 routes.post("/university", UniversityController.insert);
+routes.put("/university/:id", UniversityController.update);
+routes.delete("/university/:id", UniversityController.remove);
 
 module.exports = routes; 
