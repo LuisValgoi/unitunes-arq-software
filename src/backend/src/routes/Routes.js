1ff967e599b97ab1d7bb6b4ebfb523047ac7a2e4
Luis Valgoi <luis.valgoi@sap.com>
2019-11-20 10:05:39 -0300

    added routes to account routing

--- src/backend/src/routes/Routes.js
+++ src/backend/src/routes/Routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const UniversityRoutes = require('./UniversityRoutes');
 const AcademicRoutes = require('./AcademicRoutes');
+const AccountRoutes = require('./AccountRoutes');
 
 const routes = express.Router();
 
 routes.use('/university', UniversityRoutes);
 routes.use('/academic', AcademicRoutes);
+routes.use('/account', AccountRoutes);
 
 module.exports = routes; 
