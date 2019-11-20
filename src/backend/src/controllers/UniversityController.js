a0e08c12e045b7b43b88f83cfcabc71849121216
Luis Valgoi <luis.valgoi@sap.com>
2019-11-20 08:14:05 -0300

    added try catch block statements

--- src/backend/src/controllers/UniversityController.js
+++ src/backend/src/controllers/UniversityController.js
@@ -2,52 +2,64 @@ const University = require('../models/University');
 
 module.exports = {
   async getAll(req, res) {
-    return res.json(await University.find());
+    try {
+      return res.json(await University.find());
+    } catch (e) {
+      console.log(e)
+      res.status(500).send(e);
+    }
   },
 
   async getById(req, res) {
-    const { id } = req.params;
-    const response = await University.findById(id);
-    return res.json(response);
+    try {
+      const { id } = req.params;
+      const response = await University.findById(id);
+      return res.json(response);
+    } catch (e) {
+      console.log(e)
+      res.status(500).send(e);
+    }
   },
 
   async insert(req, res) {
-    const {
-      name, country, state, address,
-    } = req.body;
-    const record = await University.create({
-      name,
-      country,
-      state,
-      address,
-    });
-
-    return res.json(record);
-  },
-
-  async update(req, res) {
-    const { id } = req.params;
-    const {
-      name, country, state, address,
-    } = req.body;
-    const record = await University.findByIdAndUpdate(
-      id,
-      {
+    try {
+      const { name, country, state, address } = req.body;
+      const record = await University.create({
         name,
         country,
         state,
         address,
-      },
-      { new: true },
-    );
+      });
 
-    return res.json(record);
+      return res.json(record);
+    } catch (e) {
+      console.log(e)
+      res.status(500).send(e);
+    }
+  },
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const { name, country, state, address } = req.body;
+      const record = await University.findByIdAndUpdate(id, { name, country, state, address }, { new: true });
+
+      return res.json(record);
+    } catch (e) {
+      console.log(e)
+      res.status(500).send(e);
+    }
   },
 
   async remove(req, res) {
-    const { id } = req.params;
-    const record = await University.findByIdAndDelete(id);
+    try {
+      const { id } = req.params;
+      const record = await University.findByIdAndDelete(id);
 
-    return res.json(record);
+      return res.json(record);
+    } catch (e) {
+      console.log(e)
+      res.status(500).send(e);
+    }
   },
 };
