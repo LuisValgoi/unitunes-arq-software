44583d6284b2299ed51aebf6db018a31740cfd62
Luis Valgoi <luis.valgoi@sap.com>
2019-11-20 08:15:12 -0300

    fixed identation

--- src/backend/src/controllers/AcademicController.js
+++ src/backend/src/controllers/AcademicController.js
@@ -1,58 +1,53 @@
 const Academic = require('../models/Academic');
 
 module.exports = {
-  async getAll (req, res) {
+  async getAll(req, res) {
     try {
       return res.json(await Academic.find());
-    }
-    catch (e) {
+    } catch (e) {
       console.log(e)
       res.status(500).send(e);
-    } 
+    }
   },
 
-  async getById (req, res) {
+  async getById(req, res) {
     try {
       const { id } = req.params;
       const response = await Academic.findById(id)
       return res.json(response);
-    }
-    catch (e) {
+    } catch (e) {
       console.log(e)
       res.status(500).send(e);
     }
   },
 
-  async insert (req, res) {
+  async insert(req, res) {
     try {
       const record = await Academic.create(req.body);
       return res.json(record)
-    } 
-    catch (e) {
+    } catch (e) {
       console.log(e)
       res.status(500).send(e);
     }
   },
 
-  async update (req, res) {
+  async update(req, res) {
     try {
       const { id } = req.params;
       const record = await University.findByIdAndUpdate(id, req.body, { new: true });
       return res.json(record);
-    }
-    catch (e) {
+    } catch (e) {
       console.log(e)
       res.status(500).send(e);
     }
   },
 
-  async remove (req, res) {
+  async remove(req, res) {
     try {
       const { id } = req.params;
       const record = await University.findByIdAndDelete(id);
       return res.json(record);
-    }
-    catch (e) {
+    } catch (e) {
       console.log(e)
       res.status(500).send(e);
     }
