d7f0bca5a169df28bdd5fb9fc0833b3ff4e2d85e
Luis Valgoi <luis.valgoi@sap.com>
2019-11-22 18:58:09 -0300

    added basecontroller which calls e

--- src/backend/src/controllers/BaseController.js
+++ src/backend/src/controllers/BaseController.js
@@ -1,59 +1,25 @@
-module.exports = function (Entity) {
+module.exports = function (Service) {
   let module = {};
 
   module.getAll = async function (req, res) {
-    try {
-      return res.json(await Entity.find());
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
+    Service.getAll(req, res);
   }
 
   module.getById = async function (req, res) {
-    try {
-      const { id } = req.params;
-      const response = await Entity.findById(id);
-      return res.json(response);
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
+    Service.getById(req, res);
   },
 
-    module.insert = async function (req, res) {
-      try {
-        const record = await Entity.create(req.body);
-        return res.json(record);
-      } catch (e) {
-        console.log(e);
-        res.status(500).send(e);
-      }
-    },
+  module.insert = async function (req, res) {
+    Service.getById(req, res);
+  },
 
-    module.update = async function (req, res) {
-      try {
-        const { id } = req.params;
-        const record = await Entity.findByIdAndUpdate(id, req.body, { new: true });
+  module.update = async function (req, res) {
+    Service.update(req, res);
+  },
 
-        return res.json(record);
-      } catch (e) {
-        console.log(e);
-        res.status(500).send(e);
-      }
-    },
-
-    module.remove = async function (req, res) {
-      try {
-        const { id } = req.params;
-        const record = await Entity.findByIdAndDelete(id);
-
-        return res.json(record);
-      } catch (e) {
-        console.log(e);
-        res.status(500).send(e);
-      }
-    }
+  module.remove = async function (req, res) {
+    Service.remove(req, res);
+  }
 
   return module;
 };
