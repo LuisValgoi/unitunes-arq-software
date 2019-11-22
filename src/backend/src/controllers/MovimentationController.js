b25ea3bb2052aee377236b6f07bdd4f99e066f05
Luis Valgoi <luis.valgoi@sap.com>
2019-11-22 01:07:25 -0300

    extended basecontroller class for movimentation

--- src/backend/src/controllers/MovimentationController.js
+++ src/backend/src/controllers/MovimentationController.js
@@ -1,66 +1,36 @@
 const Movimentation = require('../models/Movimentation');
+const BaseController = require("./BaseController")(Movimentation);
 
-module.exports = {
+BaseController.getAllByAccount = async function (req, res) {
+  try {
+    const { fromDate, toDate, seller, buyer } = req.query;
+    let account = req.params.id;
+    let query = { account };
 
-  async getAll(req, res) {
-    try {
-      const { fromDate, toDate, seller, buyer } = req.query;
-      let account = req.params.id;
-      let query = { account };
+    if (fromDate && toDate)
+      query['createdAt'] = { '$gte': fromDate, '$lt': toDate }
 
-      if (fromDate && toDate)
-        query['createdAt'] = { '$gte': fromDate, '$lt': toDate }
+    if (seller)
+      query['seller'] = seller
 
-      if (seller)
-        query['seller'] = seller
+    if (buyer)
+      query['buyer'] = buyer
 
-      if (buyer)
-        query['buyer'] = buyer
-
-      return res.json(await Movimentation.find(query));
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
-  },
-
-  async _getMediasByUser(userId) {
-    try {
-      let query = { "buyer": userId };
-      return res.json(await Movimentation.find(query).select("medias"));
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
-  },
-
-  async getById(req, res) {
-    try {
-      const { id } = req.params;
-      return res.json(await Movimentation.findById(id))
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
-  },
-
-  async insert(req, res) {
-    try {
-      const record = await Movimentation.create(req.body);
-      return res.json(record);
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
-  },
-
-  async update(req, res) {
-    try {
-      const record = await Movimentation.findByIdAndUpdate(id, req.body, { new: true });
-      return res.json(record);
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
+    return res.json(await Movimentation.find(query));
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
   }
 };
+
+BaseController._getMediasByUser = async function (userId) {
+  try {
+    let query = { "buyer": userId };
+    return res.json(await Movimentation.find(query).select("medias"));
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+};
+
+module.exports = BaseController;
