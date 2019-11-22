36b3aeb977a03b56bee79fbde7767724d1978d9e
Luis Valgoi <luis.valgoi@sap.com>
2019-11-22 00:54:45 -0300

    extended basecontroller class for academic

--- src/backend/src/controllers/AcademicController.js
+++ src/backend/src/controllers/AcademicController.js
@@ -1,56 +1,15 @@
 const Academic = require('../models/Academic');
+const BaseController = require("./BaseController")(Academic);
 
-module.exports = {
-  async getAll(req, res) {
-    try {
-      return res.json(await Academic.find());
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
-  },
-
-  async getById(req, res) {
-    try {
-      const { id } = req.params;
-      const response = await Academic.findById(id)
-      return res.json(response);
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
-  },
-
-  async insert(req, res) {
-    try {
-      const record = await Academic.create(req.body);
-      // TODO: Create an account
-      return res.json(record)
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
-  },
-
-  async update(req, res) {
-    try {
-      const { id } = req.params;
-      const record = await Academic.findByIdAndUpdate(id, req.body, { new: true });
-      return res.json(record);
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
-  },
-
-  async remove(req, res) {
-    try {
-      const { id } = req.params;
-      const record = await Academic.findByIdAndDelete(id);
-      return res.json(record);
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
+BaseController.insert = async function () { 
+  try {
+    const record = await Academic.create(req.body);
+    // TODO: Create an account
+    return res.json(record)
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
   }
-};
+}
+
+module.exports = BaseController;
