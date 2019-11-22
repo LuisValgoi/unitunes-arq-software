771dd63f9d1d64cd934d652d7237373e8fd3b8fa
Luis Valgoi <luis.valgoi@sap.com>
2019-11-22 00:43:56 -0300

    created a basecontroller

---
+++ src/backend/src/controllers/BaseController.js
@@ -0,0 +1,58 @@
+module.exports = function (Entity) {
+  let module = { };
+  module.getAll = async function (req, res) {
+    try {
+      return res.json(await Entity.find());
+    } catch (e) {
+      console.log(e);
+      res.status(500).send(e);
+    }
+  }
+
+  module.getById = async function (req, res) {
+    try {
+      const { id } = req.params;
+      const response = await Entity.findById(id);
+      return res.json(response);
+    } catch (e) {
+      console.log(e);
+      res.status(500).send(e);
+    }
+  },
+
+  module.insert = async function (req, res) {
+    try {
+      const record = await Entity.create(req.body);
+      return res.json(record);
+    } catch (e) {
+      console.log(e);
+      res.status(500).send(e);
+    }
+  },
+
+  module.update = async function (req, res) {
+    try {
+      const { id } = req.params;
+      const record = await Entity.findByIdAndUpdate(id, req.body, { new: true });
+
+      return res.json(record);
+    } catch (e) {
+      console.log(e);
+      res.status(500).send(e);
+    }
+  },
+
+  module.remove = async function (req, res) {
+    try {
+      const { id } = req.params;
+      const record = await Entity.findByIdAndDelete(id);
+
+      return res.json(record);
+    } catch (e) {
+      console.log(e);
+      res.status(500).send(e);
+    }
+  }
+
+  return module;
+};
