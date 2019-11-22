2577378a87002091a6aec661433f5024287ecc82
Luis Valgoi <luis.valgoi@sap.com>
2019-11-22 00:54:34 -0300

    improved code @ basecontroller

--- src/backend/src/controllers/BaseController.js
+++ src/backend/src/controllers/BaseController.js
@@ -1,5 +1,6 @@
 module.exports = function (Entity) {
-  let module = { };
+  let module = {};
+
   module.getAll = async function (req, res) {
     try {
       return res.json(await Entity.find());
@@ -20,39 +21,39 @@ module.exports = function (Entity) {
     }
   },
 
-  module.insert = async function (req, res) {
-    try {
-      const record = await Entity.create(req.body);
-      return res.json(record);
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
+    module.insert = async function (req, res) {
+      try {
+        const record = await Entity.create(req.body);
+        return res.json(record);
+      } catch (e) {
+        console.log(e);
+        res.status(500).send(e);
+      }
+    },
+
+    module.update = async function (req, res) {
+      try {
+        const { id } = req.params;
+        const record = await Entity.findByIdAndUpdate(id, req.body, { new: true });
+
+        return res.json(record);
+      } catch (e) {
+        console.log(e);
+        res.status(500).send(e);
+      }
+    },
+
+    module.remove = async function (req, res) {
+      try {
+        const { id } = req.params;
+        const record = await Entity.findByIdAndDelete(id);
+
+        return res.json(record);
+      } catch (e) {
+        console.log(e);
+        res.status(500).send(e);
+      }
     }
-  },
-
-  module.update = async function (req, res) {
-    try {
-      const { id } = req.params;
-      const record = await Entity.findByIdAndUpdate(id, req.body, { new: true });
-
-      return res.json(record);
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
-  },
-
-  module.remove = async function (req, res) {
-    try {
-      const { id } = req.params;
-      const record = await Entity.findByIdAndDelete(id);
-
-      return res.json(record);
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
-  }
 
   return module;
 };
