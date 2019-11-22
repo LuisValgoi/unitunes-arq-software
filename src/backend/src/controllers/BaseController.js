a1c556a34681b8b0744030a38e7ac50c42fe13de
Luis Valgoi <luis.valgoi@sap.com>
2019-11-22 19:08:51 -0300

    added return into basecontroller

--- src/backend/src/controllers/BaseController.js
+++ src/backend/src/controllers/BaseController.js
@@ -2,23 +2,23 @@ module.exports = function (Service) {
   let module = {};
 
   module.getAll = async function (req, res) {
-    Service.getAll(req, res);
+    return Service.getAll(req, res);
   }
 
   module.getById = async function (req, res) {
-    Service.getById(req, res);
+    return Service.getById(req, res);
   },
 
   module.insert = async function (req, res) {
-    Service.insert(req, res);
+    return Service.insert(req, res);
   },
 
   module.update = async function (req, res) {
-    Service.update(req, res);
+    return Service.update(req, res);
   },
 
   module.remove = async function (req, res) {
-    Service.remove(req, res);
+    return Service.remove(req, res);
   }
 
   return module;
