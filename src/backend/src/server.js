ae62f42630a06bb56000e40ff634a62ee7e125b1
Luis Valgoi <luis.valgoi@sap.com>
2019-11-23 11:22:54 -0300

    added possibility to debug

--- src/backend/src/server.js
+++ src/backend/src/server.js
@@ -16,4 +16,21 @@ try {
 server.use(cors());
 server.use(express.json());
 server.use(routes);
+
+// catch 404 and forward to error handler
+server.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+server.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 server.listen(3333);
