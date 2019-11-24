1a0ba55067fa0e2cec828bd0ee32540148f3cc66
Luis Valgoi <luis.valgoi@sap.com>
2019-11-24 11:58:49 -0300

    removed old code

--- src/backend/src/controllers/AppController.js
+++ src/backend/src/controllers/AppController.js
@@ -1,8 +1,5 @@
 module.exports = {
   root(req, res) {
-    console.log('Inside the homepage callback function');
-    console.log("sessionID", req.sessionID);
-
     const availableAPIs = [
       '/university',
       '/academic',
@@ -15,14 +12,8 @@ module.exports = {
     res.json(availableAPIs);
   },
   loginGet(req, res) {
-    console.log('Inside GET /login callback function')
-    console.log(req.sessionID)
-    res.send(`You got the login page!\n`)
   },
   loginPost(req, res) {
-    console.log('Inside POST /login callback function')
-    console.log(req.body)
-    res.send(`You posted to the login page!\n`)
   },
   logout(req, res) {
   }
