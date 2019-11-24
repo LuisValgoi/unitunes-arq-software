106fae1ea83b1523b9bf7df3ce8648d43f01169b
Luis Valgoi <luis.valgoi@sap.com>
2019-11-24 18:18:23 -0300

    removed unused code

--- src/backend/src/models/User.js
+++ src/backend/src/models/User.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const { Schema, model } = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
 mongoose.set('useCreateIndex', true);
 mongoose.pluralize(null);
@@ -54,38 +53,5 @@ UserSchema.pre('save', async function (next) {
   }
   next();
 });
-
-// UserSchema.pre('findOneAndUpdate', async function (next) {
-//   // Hash the password before saving the user model
-//   const user = this._update;
-//   if (user.isModified('password')) {
-//     user.password = await bcrypt.hash(user.password, 8);
-//   }
-//   next();
-// });
-
-UserSchema.methods.generateAuthToken = async function () {
-  // Generate an auth token for the user
-  const user = this;
-  const token = jwt.sign({ _id: user._id }, process.env.JWT_KEY);
-  user.tokens = user.tokens.concat({ token });
-  await user.save();
-  return token
-};
-
-UserSchema.statics.findByCredentials = async (email, password) => {
-  // Search for a user by email and password.
-  const user = await User.findOne({ email })
-  if (!user) {
-    throw new Error({ error: 'Invalid login credentials' })
-  }
-
-  const isPasswordMatch = await bcrypt.compare(password, user.password)
-  if (!isPasswordMatch) {
-    throw new Error({ error: 'Invalid login credentials' })
-  }
-
-  return user
-};
 
 module.exports = model('User', UserSchema);
