eb608c3f139954cc3a63474764446c3ea815fc5f
Luis Valgoi <luis.valgoi@sap.com>
2019-11-24 11:19:52 -0300

    added more apis to app routing

--- src/backend/src/controllers/AppController.js
+++ src/backend/src/controllers/AppController.js
@@ -14,7 +14,15 @@ module.exports = {
 
     res.json(availableAPIs);
   },
-  login(req, res) {
+  loginGet(req, res) {
+    console.log('Inside GET /login callback function')
+    console.log(req.sessionID)
+    res.send(`You got the login page!\n`)
+  },
+  loginPost(req, res) {
+    console.log('Inside POST /login callback function')
+    console.log(req.body)
+    res.send(`You posted to the login page!\n`)
   },
   logout(req, res) {
   }
--- src/backend/src/routes/AppRoutes.js
+++ src/backend/src/routes/AppRoutes.js
@@ -3,7 +3,8 @@ const routes = express.Router();
 const AppController = require('../controllers/AppController');
 
 routes.get('/', AppController.root);
-routes.post('/login', AppController.login);
+routes.post('/login', AppController.loginPost);
+routes.get('/login', AppController.loginGet);
 routes.post('/logout', AppController.logout);
 
 module.exports = routes; 
