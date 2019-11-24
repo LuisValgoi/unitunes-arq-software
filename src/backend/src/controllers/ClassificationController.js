afa5a94abad876412e9dea963106c10dc43456f7
vdschuck <vinicius.schuck@live.com>
2019-11-24 14:29:34 -0300

    Added endpoint to classification

---
+++ src/backend/src/controllers/ClassificationController.js
@@ -0,0 +1,28 @@
+const ClassificationService = require('../services/ClassificationService');
+const BaseController = require('./BaseController')(ClassificationService);
+
+BaseController.getAllByMedia = async function (req, res) {
+  try {
+    let id = req.params.id;
+    let data = await ClassificationService.getAllByMedia(id);
+
+    return res.json(data);
+  } catch (e) {
+    console.log("Reported Error:", e);
+    res.status(500).send(e);
+  }
+};
+
+BaseController.getScoreByMedia = async function (req, res) {
+  try {
+    let id = req.params.id;
+    let data = await ClassificationService.getScoreByMedia(id);
+
+    return res.json(data);
+  } catch (e) {
+    console.log("Reported Error:", e);
+    res.status(500).send(e);
+  }
+};
+
+module.exports = BaseController;
---
+++ src/backend/src/services/ClassificationService.js
@@ -0,0 +1,32 @@
+const Classification = require('../models/Classification');
+const BaseService = require('./BaseService')(Classification);
+
+BaseService.getAllByMedia = async function (id) {
+  try {
+    let query = { 'media': id };
+
+    return await Classification.find(query);
+  } catch (e) {
+    console.log("Reported Error:", e);
+  }
+};
+
+BaseService.getScoreByMedia = async function (id) {
+  try {
+    let query = { 'media': id };
+    let result = await Classification.find(query).select(['value']);
+    let count = 0
+
+    for (let item of result) {
+      count += item['value']
+    }
+
+    return count / result.length;
+  } catch (e) {
+    console.log("Reported Error:", e);
+  }
+};
+
+
+
+module.exports = BaseService;
