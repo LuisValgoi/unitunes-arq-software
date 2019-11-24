d47dfe49f6fb57ae6fbaa7ab1104f97e3a618486
Luis Valgoi <luis.valgoi@sap.com>
2019-11-24 16:34:51 -0300

    added api which exposes all classifications

--- src/backend/src/routes/ClassificationRoutes.js
+++ src/backend/src/routes/ClassificationRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = express.Router();
 const ClassificationController = require('../controllers/ClassificationController');
 
+routes.get('/', ClassificationController.getAll);
 routes.get('/:id', ClassificationController.getById);
 routes.get('/media/:id', ClassificationController.getAllByMedia);
 routes.get('/media/:id/score', ClassificationController.getScoreByMedia);
