e4c46d16f73dcb414074f6e65167ef2510461b0c
Luis Valgoi <luis.valgoi@sap.com>
2019-11-24 18:19:55 -0300

    exposed method to be used when creating a user

--- src/backend/src/services/AccountService.js
+++ src/backend/src/services/AccountService.js
@@ -9,4 +9,16 @@ BaseService.viewCredit = async function (id) {
   }
 };
 
+BaseService.generateAccountIdForUserCreation = async function () {
+  try {
+    let payload = {
+      currentAmount: 0
+    };
+    let data = await Account.create(payload);
+    return data._id;
+  } catch (e) {
+    console.log('Reported Error:', e);
+  }
+};
+
 module.exports = BaseService;
