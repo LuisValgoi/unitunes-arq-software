f44f718558a2ed6dffb2ea4cbec5d3e3bd226cde
Luis Valgoi <luis.valgoi@sap.com>
2019-11-23 21:33:31 -0300

    fixed responsabilities between layers for library

--- src/backend/src/controllers/LibraryController.js
+++ src/backend/src/controllers/LibraryController.js
@@ -2,7 +2,15 @@ const LibraryService = require('../services/LibraryService');
 const BaseController = require('./BaseController')(LibraryService);
 
 BaseController.getAllByUser = async function (req, res) {
-  return LibraryService.getAllByUser(req, res);
+  try {
+    let userId = req.params.id;
+    let data = await LibraryService.getAllByUser(userId);
+
+    return res.json(data);
+  } catch (e) {
+    console.log("Reported Error:", e);
+    res.status(500).send(e);
+  }
 };
 
 module.exports = BaseController;
--- src/backend/src/services/LibraryService.js
+++ src/backend/src/services/LibraryService.js
@@ -2,23 +2,19 @@ const FavoriteService = require('./FavoriteService');
 const MediaService = require('./MediaService');
 
 module.exports = {
-  async getAllByUser (req, res) {
+  async getAllByUser(userId) {
     try {
-      const { userId } = req.params;
-
-      let favorites = await FavoriteService._getAllByUser(req, res);
-      let medias = await MediaService._getAllByUser(req, res);
+      let favorites = await FavoriteService.getAllByUser(userId);
+      let medias = await MediaService.getAllByUser(userId);
       // TODO: Get all albums by user
 
-      let response = {
+      return {
         'medias': medias,
         'albums': null,
         'favorites': favorites
-      }
-      return res.json(response);
+      };
     } catch (e) {
-        console.log(e);
-        res.status(500).send(e);
+      console.log("Reported Error:", e);
     }
   }
 }
