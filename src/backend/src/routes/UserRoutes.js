dcd96363e32c03d40dfc2adcabb8a2d0a9fd6598
Luis Valgoi <luis.valgoi@sap.com>
2019-11-24 20:09:10 -0300

    fixed bug by reordering

--- src/backend/src/routes/UserRoutes.js
+++ src/backend/src/routes/UserRoutes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 const auth = require('../middleware/auth');
 const UserController = require('../controllers/UserController');
 
+routes.get('/me', auth, UserController.me);
 routes.get('/', UserController.getAll);
 routes.get('/:id', UserController.getById);
 routes.post('/', UserController.insert);
@@ -10,16 +11,16 @@ routes.put('/:id', UserController.update);
 routes.delete('/:id', UserController.remove);
 routes.post('/password/recovery', UserController.recoverPassword);
 routes.post('/login', UserController.login);
-routes.get('/me', auth, UserController.me);
 
 module.exports = routes; 
 
 /*
+getMe               - retorarEu
 getAll              - retornarTodos
 getById             - retornar
 insert              - criar
 update              - atualizar
 remove              - deletar
-authenticate        - autenticar
 recoverPassword     - recuperarSenha
+login               - autenticar
 */
