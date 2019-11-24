bb46ddac354d1260449102a89384d06527f80738
Luis Valgoi <luis.valgoi@sap.com>
2019-11-24 20:08:59 -0300

    fixed middleware

--- src/backend/src/middleware/auth.js
+++ src/backend/src/middleware/auth.js
@@ -1,20 +1,20 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-const auth = async (req, res, next) => {
-  const token = req.header('Authorization').replace('Bearer ', '');
-  const data = jwt.verify(token, process.env.JWT_KEY);
+const auth = async function (req, res, next) {
   try {
-    const user = await User.findOne({ _id: data._id, 'tokens.token': token });
+    let token = req.header('Authorization').replace('Bearer ', '');
+    let data = jwt.verify(token, process.env.JWT_KEY);
+    let user = await User.findById(data._id);
     if (!user) {
       throw new Error('UserNotFound');
     }
+
     req.user = user;
     req.token = token;
     next();
   } catch (error) {
     res.status(401).send({ error: 'Not authorized to access this resource' })
   }
-
 }
-module.exports = auth
+module.exports = auth;
