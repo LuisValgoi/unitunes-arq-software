109e71f146a582fe4a753c2dcf926b2beb304461
Luis Valgoi <luis.valgoi@sap.com>
2019-11-25 21:59:22 -0300

    improved readability (correct)

--- src/backend/src/services/AlbumService.js
+++ src/backend/src/services/AlbumService.js
@@ -7,15 +7,15 @@ const QueryHelper = require('../util/QueryHelper');
 BaseService.getAllNews = async function () {
   let minDate = DateHelper.getDateDeacreasedBy(new Date(), 40);
   let query = QueryHelper.getCreatedAtGreaterThan(minDate);
-  let albums = await Album.find(query);
-  return albums;
+
+  return await Album.find(query);
 };
 
 BaseService.getAllRecents = async function () {
   let minDate = DateHelper.getDateDeacreasedBy(new Date(), 60);
   let query = QueryHelper.getCreatedAtGreaterThan(minDate);
-  let albums = await Album.find(query);
-  return albums;
+
+  return await Album.find(query);
 };
 
 module.exports = BaseService;
