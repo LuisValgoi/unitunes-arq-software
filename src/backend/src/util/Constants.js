0b92151650752fd03b643bc0d8c02eeb414eaeb1
Luis Valgoi <luis.valgoi@sap.com>
2019-11-26 00:14:17 -0300

    added categories returns

--- src/backend/src/util/Constants.js
+++ src/backend/src/util/Constants.js
@@ -1,4 +1,12 @@
+const Categories = require('./Categories');
+
 module.exports = {
   PASSWORD_LENGTH: 10,
-  PASSWORD_CHARACTERS: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  PASSWORD_CHARACTERS: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+  CATEGORIES: {
+    MUSIC: Categories.getMusic(),
+    BOOK: Categories.getBook(),
+    MOVIE: Categories.getMovie(),
+    PODCAST: Categories.getPodcast(),
+  }
 };
