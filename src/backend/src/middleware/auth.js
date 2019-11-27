c231b9ad114768125461833e78e49000ba80fd8c
Luis Valgoi <luis.valgoi@sap.com>
2019-11-27 00:12:34 -0300

    Implemented new error logic into auth middleware

--- src/backend/src/middleware/auth.js
+++ src/backend/src/middleware/auth.js
@@ -1,20 +1,31 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/User')
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const Constants = require('../util/Constants');
+const CustomError = require('../errors/CustomError');
 
 const auth = async function (req, res, next) {
-  const token = req.header('Authorization').replace('Bearer ', '');
-  const data = jwt.verify(token, process.env.JWT_KEY);
-
   try {
+    const authorizationHeader = req.header('Authorization');
+    if (!authorizationHeader) {
+      throw new CustomError.Unauthorized(Constants.EXCEPTIONS.UNAUTHORIZED);
+    }
+
+    const token = authorizationHeader.replace('Bearer ', '');
+    const data = jwt.verify(token, process.env.JWT_KEY);
     let user = await User.findOne({ _id: data._id, 'tokens.token': token });
     if (!user) {
-      throw new Error('UserNotFoundException');
+      throw new CustomError.NotFound(Constants.EXCEPTIONS.NOT_FOUND);
     }
+
     req.user = user;
     req.token = token;
+
     next();
-  } catch (error) {
-    res.status(401).send({ error: 'Not authorized to access this resource' });
+  } catch (e) {
+    res.status(e.code).send({
+      code: e.code,
+      message: e.message
+    });
   }
 }
 module.exports = auth;
