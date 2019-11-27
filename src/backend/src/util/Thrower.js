e6c3172624f42d38e9bdd005b0a8df851dd451da
Luis Valgoi <luis.valgoi@sap.com>
2019-11-27 01:51:04 -0300

    added if which prevents bugs into thrower

--- src/backend/src/util/Thrower.js
+++ src/backend/src/util/Thrower.js
@@ -1,8 +1,8 @@
 class Thrower {
   static redirect(res, error) {
-    res.status(error.code).send({
-      code: error ? error.code : 0,
-      message: error ? error.message : 'N/A'
+    res.status(error & error.code ? error.code : 500).send({
+      code: error & error.code ? error.code : 500,
+      message: error && error.message ? error.message : 'N/A'
     });
   }
 }
