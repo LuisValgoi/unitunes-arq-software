e80dda78a3e61de5210988ace0bae76012439dd8
vinicius-schuck <vinicius.schuck@eldorado.org.br>
2019-11-27 10:56:52 -0300

    Adjustments in library methods

--- src/backend/src/controllers/LibraryController.js
+++ src/backend/src/controllers/LibraryController.js
@@ -12,8 +12,21 @@ BaseController.getAllByUser = async function (req, res) {
   }
 };
 
+BaseController.getById = async function (req, res) {
+  try {
+    let userId = req.user['_id'];
+    let mediaId = req.params.id;
+    let data = await LibraryService.getById(userId, mediaId);
+
+    return res.json(data);
+  } catch (e) {
+    Thrower.redirect(res, e);
+  }
+};
+
 module.exports = BaseController;
 
 /*
 getAllByUser          - retornaMidiasCompradasPorUsuario
+getById               - retornaUmaMidiaCompradaPorUsuario
 */
