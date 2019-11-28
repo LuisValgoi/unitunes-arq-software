4758b408581e5cd79744d5b1ffafe2ec157aae07
Luis Valgoi <luis.valgoi@sap.com>
2019-11-28 00:22:50 -0300

    removed merge leftover

--- src/backend/src/routes/UserRoutes.js
+++ src/backend/src/routes/UserRoutes.js
@@ -4,7 +4,6 @@ const UserController = require('../controllers/UserController');
 
 const auth = require('../middleware/auth');
 
-routes.post('/', auth, UserController.insert);
 routes.get('/me', auth, UserController.me);
 routes.post('/me/logout', auth, UserController.logout);
 routes.post('/me/logoutAll', auth, UserController.logoutAll);
@@ -22,7 +21,7 @@ module.exports = routes;
 getMe               - retorarEu
 getAll              - retornarTodos
 getById             - retornar
-insert              - criar
+signUp              - criar
 update              - atualizar
 remove              - deletar
 recoverPassword     - recuperarSenha
