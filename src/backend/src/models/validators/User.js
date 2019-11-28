63b7001e6549a055f653dc731608ed159e51c466
Luis Valgoi <luis.valgoi@sap.com>
2019-11-28 00:18:45 -0300

    improved property access to user validator

--- src/backend/src/models/validators/User.js
+++ src/backend/src/models/validators/User.js
@@ -1,11 +1,11 @@
 module.exports = {
   validateUser(user) {
-    if (!user || user['active'] == false) {
+    if (!user || user.active == false) {
       throw new NotFoundError(Constants.EXCEPTIONS.USER_NOT_FOUND);
     }
   },
   validateUserAdmin(user) {
-    if (user['role'] != 'admin') {
+    if (user.role !== 'admin') {
       throw new UnauthorizedError(Constants.EXCEPTIONS.UNAUTHORIZED);
     }
   },
