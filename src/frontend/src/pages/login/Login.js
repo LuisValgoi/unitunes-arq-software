ece990e9fc7820ab0e6c663189b6224bd286f65f
Luis Valgoi <luis.valgoi@sap.com>
2019-12-02 20:27:26 -0300

    added partial login page w/ style

--- src/frontend/src/pages/login/Login.js
+++ src/frontend/src/pages/login/Login.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "./Login.css";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/logo.png";
 
 function Login() {
   return (
     <div className="login">
       <form className="form" method="post">
         <img src={logo} className="logo" alt="logo" />
-        <h1 className="title">Entre na sua Conta</h1>
         <div className="container">
           <input type="email" placeholder="E-mail"></input>
           <input type="password" placeholder="Senha"></input>
           <button type="submit">Entrar</button>
-          <label>Esqueceu sua senha?</label>
+          <div className="forgotPassword">
+            <label>Esqueceu sua senha?</label>
+            <label><a href="">Clique aqui!</a></label>
+          </div>
+        </div>
+        <div class="register">
+
         </div>
       </form>
     </div>
