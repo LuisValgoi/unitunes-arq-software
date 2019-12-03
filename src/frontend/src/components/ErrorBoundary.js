8c846a90aa361d6ded413c99764ce7790252703c
Luis Valgoi <luis.valgoi@sap.com>
2019-12-03 20:46:44 -0300

    fixed error boundary

--- src/frontend/src/components/ErrorBoundary.js
+++ src/frontend/src/components/ErrorBoundary.js
@@ -3,23 +3,32 @@ import React, { Component } from "react";
 class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      hasError: false
-    };
+    this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError() {
+  static getDerivedStateFromError(error) {
+    alert("getDerivedStateFromError")
+    return { hasError: true };
+  }
+
+  componentDidCatch(error) {
+    alert("componentDidCatch")
     this.setState({
       hasError: true
-    })
+    });
   }
 
   render() {
     if (this.state.hasError) {
       return (
-        <h2>Something went wrong.</h2>
+        <h1>
+          Unable to load the Content...
+            <br />
+          Please refresh the page and try again...!
+        </h1>
       );
     }
+
     return this.props.children;
   }
 }
