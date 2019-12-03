a7753c7f8028c9dc24e31a9a9077bcdc903ab3ee
Luis Valgoi <luis.valgoi@sap.com>
2019-12-03 19:54:48 -0300

    reordered

--- src/frontend/src/components/auth/login/Login.js
+++ src/frontend/src/components/auth/login/Login.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "../Auth.css";
 import logo from "../../../assets/logo.png";
+import urls from "../../../services/urls";
+import api from "../../../services/api";
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    await api.post(urls.LOGIN, {
+      email,
+      password
+    }).catch(e => {
+      throw new Error(e.response.data.message);
+    });
+  }
+
   return (
     <div className="page">
-      <form className="form" method="post">
+      <form className="form" onSubmit={onSubmit} method="post">
         <Link to="/">
           <img src={logo} className="logo" alt="logo" />
         </Link>
         <div className="container">
-          <input type="email" placeholder="E-mail"></input>
-          <input type="password" placeholder="Senha"></input>
+          <input type="email" name="email" placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} />
+          <input type="password" name="password" placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)} />
           <button type="submit">Entrar</button>
         </div>
         <div className="section-01">
--- src/frontend/src/routes.js
+++ src/frontend/src/routes.js
@@ -2,19 +2,31 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-import Home from "./pages/home/Home";
-import Login from "./pages/auth/login/Login";
-import Register from "./pages/auth/register/Register";
-import Reset from "./pages/auth/password/Reset";
+import Home from "./components/home/Home";
+import Login from "./components/auth/login/Login";
+import Register from "./components/auth/register/Register";
+import Reset from "./components/auth/password/Reset";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function Routes() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/reset" component={Reset} />
-    </BrowserRouter>
+      <ErrorBoundary>
+        <Route exact path="/" component={Home} />
+      </ErrorBoundary>
+
+      <ErrorBoundary>
+        <Route exact path="/login" component={Login} />
+      </ErrorBoundary>
+
+      <ErrorBoundary>
+        <Route exact path="/register" component={Register} />
+      </ErrorBoundary>
+
+      <ErrorBoundary>
+        <Route exact path="/reset" component={Reset} />
+      </ErrorBoundary>
+    </BrowserRouter >
   );
 }
 
