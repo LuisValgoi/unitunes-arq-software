eb7be18a7bdecb0ba130d2fd60edcdf623ec87c9
Luis Valgoi <luis.valgoi@sap.com>
2019-12-02 21:29:23 -0300

    added reset routing

---
+++ src/frontend/src/pages/password/Reset.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Reset.css";
+
+function Reset() {
+  return (
+    <h1>Reset</h1>
+  );
+}
+
+export default Reset;
--- src/frontend/src/routes.js
+++ src/frontend/src/routes.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
+import Reset from "./pages/password/Reset";
 
 function Routes() {
   return (
@@ -12,6 +13,7 @@ function Routes() {
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
+      <Route exact path="/password/reset" component={Reset} />
     </BrowserRouter>
   );
 }
