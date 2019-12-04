0b59672cc4a66f72f0440baf3b88becf979b679c
Luis Valgoi <luis.valgoi@sap.com>
2019-12-04 00:14:29 -0300

    added partial logic for reset elements

--- src/frontend/src/components/auth/password/Reset.js
+++ src/frontend/src/components/auth/password/Reset.js
@@ -1,26 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "../Auth.css";
+import "./Register.css";
 import logo from "../../../assets/logo.png";
+import urls from "../../../services/urls";
+import api from "../../../services/api";
 
 function Reset() {
+  const [email, setEmail] = useState('');
+  const [emailSent, setEmailSent] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    await api.post(urls.RESET, {
+      email
+    }).then(() => {
+      setEmailSent(true);
+      setError(false);
+      setErrorMsg('');
+    }).catch(e => {
+      setEmailSent(false);
+      setError(true);
+      setErrorMsg(e.response.data.message);
+    }).finally(e => {
+      setIsLoading(false);
+    });
+  }
+
+  function onChangeEmail(value) {
+    setEmail(value);
+    setEmailSent(false);
+    setError(false);
+    setErrorMsg('');
+  }
+
+  let sucessHTML = (
+    <div className="section-02">
+      <label>O usuário receberá um e-mail com a nova senha.</label>
+      <label>
+        <Link to="/login">Faça Login</Link>
+      </label>
+    </div>
+  );
+
+  let defaultHTML = (
+    <div className="section-02">
+      <label>Lembrou de sua senha?</label>
+      <label>
+        <Link to="/login">Faça Login</Link>
+      </label>
+    </div>
+  );
+
+  let errorHTML = (
+    <div className="section-02 error">
+      <label className="error">{errorMsg}</label>
+      <label>
+        <Link to="/login">Voltar</Link>
+      </label>
+    </div>
+  );
+
+  let block = defaultHTML;
+  if (error) {
+    block = errorHTML;
+  } else if (emailSent) {
+    block = sucessHTML;
+  }
+
   return (
     <div className="page">
-      <form className="form" method="post">
+      <form className="form" onSubmit={onSubmit} method="post">
         <Link to="/">
           <img src={logo} className="logo" alt="logo" />
         </Link>
         <div className="container">
-          <input type="email" placeholder="E-mail"></input>
-          <button type="submit">Reset</button>
+          <input type="email" name="email" required={true} placeholder="E-mail" value={email} onChange={e => onChangeEmail(e.target.value)} />
+          <button type="submit" className={isLoading ? 'is-loading' : ''}>Reset</button>
         </div>
-        <div className="section-02">
-          <label>Lembrou de sua senha?</label>
-          <label>
-            <Link to="/login">Clique aqui</Link>
-          </label>
-        </div>
+        {block}
       </form>
     </div>
   );
